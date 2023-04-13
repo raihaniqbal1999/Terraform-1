1219bc5c87bfe6df5558d5738b31efdf9032de2d
Iqbal <raihaniqbal@IN-KGSMAC54.local>
2023-04-13 15:22:44 +0530

    removing id's

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "subscription_id" {
-  default = "d3d7ddab-1de6-48df-ad7f-7220688ffb32" // FL Sandbox Subscription
+  default = "" // FL Sandbox Subscription
 }
 
 variable "tenant_id" {
-  default = "a4d4d5cd-20cf-444e-bfe7-3d3ecef3fe94" //CloudOps tenant
+  default = "" //CloudOps tenant
 }
 
 variable "environment" {
